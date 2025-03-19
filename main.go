c069c292e1335ab4dbeae8226d85fd18bcce467e
STones_ <61647649@qq.com>
2025-03-19 14:25:53 +0800

    空项目

--- main.go
+++ main.go
@@ -2,17 +2,6 @@ package main
 
 import "fmt"
 
-/*
-会议纪要系统：
-1. 语音识别
-2. 会议内容分析
-3. 会议纪要提炼，总结
-
-目的：
-1. 完成对AI模型的部署，运用
-2. 完成py语言的学习，并应用
-3. 完成Go与Py之间的调用
-*/
 func main() {
-	fmt.Println("hello conference")
+	fmt.Println("hello empyt proj")
 }
